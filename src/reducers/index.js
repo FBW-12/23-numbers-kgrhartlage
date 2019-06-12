5b1140bb2aea0276c1448f958a30d1cd1f85903a
kgrhartlage <webdevkonto@gmail.com>
2019-06-12 13:53:28 +0200

    Create reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,38 @@
+import {
+  NUMBERS_API_REQUEST,
+  NUMBERS_API_SUCCESS,
+  NUMBERS_API_FAILURE,
+} from '../actions';
+
+const initialState = {
+  number: null,
+  trivia: '',
+  fetching: false,
+  error: null,
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case NUMBERS_API_REQUEST:
+      return {
+        ...state,
+        trivia: '',
+        fetching: true,
+        error: null,
+      };
+    case NUMBERS_API_SUCCESS:
+      return {
+        ...state,
+        trivia: action.trivia,
+        fetching: false,
+      };
+    case NUMBERS_API_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
